ede7e7de04095a5d4949044201791d5cf063b2ac
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-12-21 20:14:45 +0100

    Remove unused method

--- src/SkillCheck.elm
+++ src/SkillCheck.elm
@@ -27,6 +27,3 @@ getSkillCheckResult skillCheck actual =
   in  if diff <= 0
         then Success (min 6 (abs (diff + 1) // 3 + 1))
         else Failure (diff * -1)
-
-sumOfDice : DiceRoll -> Int
-sumOfDice roll = roll.first + roll.second + roll.third
