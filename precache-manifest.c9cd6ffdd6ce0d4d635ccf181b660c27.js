7a3989aa8bb891669c3c19533b0eeb261c12375e
MoritzR <MoritzR@users.noreply.github.com>
2021-02-04 06:19:24 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.c9cd6ffdd6ce0d4d635ccf181b660c27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/dsa-character-sheet/favicon.ico"
+  },
+  {
+    "revision": "a893e5355f29bf511ddd1214daf9b092",
+    "url": "/dsa-character-sheet/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/dsa-character-sheet/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/dsa-character-sheet/manifest.json"
+  },
+  {
+    "revision": "b7d2cd854abf21353d45",
+    "url": "/dsa-character-sheet/static/css/main.aab52bfd.chunk.css"
+  },
+  {
+    "revision": "b7d2cd854abf21353d45",
+    "url": "/dsa-character-sheet/static/js/main.b7d2cd85.chunk.js"
+  },
+  {
+    "revision": "090470eaab156367eb12",
+    "url": "/dsa-character-sheet/static/js/runtime~main.090470ea.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/dsa-character-sheet/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
